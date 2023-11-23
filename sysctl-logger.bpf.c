8f808164d939d97d3b2540e53c4996f8a3ece12b
Michael Trapp <michael.trapp@sap.com>
2023-11-23 15:53:23 +0100

    Add bpf_rcu_read_lock()
    task->real_parent is an RCU pointer and read access to dereferenced
    task_struct data requires a bpf_rcu_read_lock.

--- sysctl-logger.bpf.c
+++ sysctl-logger.bpf.c
@@ -2,16 +2,24 @@
 // Copyright (c) 2019 Facebook
 
 #include <vmlinux.h>
+#include <linux/version.h>
 
 #include <linux/errno.h>
 #include <bpf/bpf_helpers.h>
 
 #include "sysctl-logger.h"
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,2,0)
+#define HAVE_BPF_RCU_READ_LOCK
+#endif
+
 struct {
 	__uint(type, BPF_MAP_TYPE_RINGBUF);
 	__uint(max_entries, 512 * 1024 /* 256 KB */);
 } rb SEC(".maps");
+
+void bpf_rcu_read_lock(void) __ksym;
+void bpf_rcu_read_unlock(void) __ksym;
 
 SEC("cgroup/sysctl")
 int sysctl_logger(struct bpf_sysctl *ctx)
@@ -38,9 +46,15 @@ int sysctl_logger(struct bpf_sysctl *ctx)
 #endif /* HAVE_CGROUP_CURRENT_FUNC_PROTO */
 
 	struct task_struct *parent;
+#ifdef HAVE_BPF_RCU_READ_LOCK
+        bpf_rcu_read_lock();
+#endif /*  HAVE_BPF_RCU_READ_LOCK */
 	bpf_probe_read_kernel(&parent, sizeof(parent), &current->real_parent);
 	bpf_probe_read_kernel(&event->parent_pid, sizeof(event->parent_pid), &parent->pid);
 	bpf_probe_read_kernel_str(&event->parent_comm, sizeof(event->parent_comm), &parent->comm);
+#ifdef HAVE_BPF_RCU_READ_LOCK
+        bpf_rcu_read_unlock();
+#endif /*  HAVE_BPF_RCU_READ_LOCK */
 
 	__builtin_memset(event->name, 0, sizeof(event->name));
 	ret = bpf_sysctl_get_name(ctx, event->name, sizeof(event->name), 0);
