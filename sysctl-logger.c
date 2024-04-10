7dfe20feb05b311bc7d32f35b975196940fffbab
Shung-Hsi Yu <shung-hsi.yu@suse.com>
2024-04-10 11:26:20 +0800

    Rephrase logging to clarify what event represents
    Currently sysctl-logger captures the initiation of a sysctl value
    change, but does not check whether the change actually went through, so
    it is misleading to log each event as if they succeed.
    
    While is should be possible to detect whether sysctl change succeed with
    an additional kretprobe, it is not yet done, so instead change to
    logging to make it not as misleading for now.

--- sysctl-logger.c
+++ sysctl-logger.c
@@ -50,7 +50,7 @@ int handle_ringbuf_event(void *ctx, void *data, size_t data_sz)
 	event.new_value[strcspn(event.new_value, "\n")] = 0;
 
 	if (event.truncated || strncmp(event.old_value, event.new_value, sizeof(event.new_value))) {
-		printf("%s[%d](%s[%d]) changed %s from %s to %s%s\n",
+		printf("%s[%d](%s[%d]) initated change of %s from %s to %s%s\n",
                        event.comm, event.pid, event.parent_comm, event.parent_pid,
                        event.name, ((event.old_value[0] == '\0') ? "''" : event.old_value),
                        ((event.new_value[0] == '\0') ? "''" : event.new_value), warning);
